6b9dcdbf7bf9cda45875335da082eaf10ab4c7f6
luciama2 <lucia.martindeaguilera@gmail.com>
2024-10-06 23:36:04 +0200

    ex04 and ex03 makefiles, fix some minor typo

---
+++ ex04/main.cpp
@@ -0,0 +1,17 @@
+
+/**
+ * ./program filename s1 s2
+ * 
+ * open filename
+ * copies the content to a new file, filename.replace
+ * replacing all the occurrence of s1 with s2
+ * 
+ * member functions of the class std::string are allowed 
+ * except replace
+ * 
+ */
+int main(int agc, char **argv)
+{
+	
+	return (0);
+}
