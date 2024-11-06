4732e3f881fa162fba14a52b4968158f271f3962
luciama2 <luciama2@student.42madrid.com>
2024-11-06 21:02:32 +0100

    fix(ex04: TODO output file

--- ex04/stringReplacer.cpp
+++ ex04/stringReplacer.cpp
@@ -6,50 +6,73 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:27:11 by luciama2          #+#    #+#             */
-/*   Updated: 2024/10/09 19:32:42 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/06 21:00:48 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stringReplacer.hpp"
 
-StringReplacer::StringReplacer(void){}
-StringReplacer::~StringReplacer(void){}
-StringReplacer::StringReplacer(std::string filename, std::string to_find, std::string to_replace):
-	_filename(filename), _to_find(to_find), _to_replace(to_replace){}
+StringReplacer::StringReplacer(void) {}
+StringReplacer::~StringReplacer(void) {}
+StringReplacer::StringReplacer(
+	std::string filename,
+	std::string to_find,
+	std::string to_replace) : _if_filename(filename),
+							  _to_find(to_find),
+							  _to_replace(to_replace) {}
 
-void StringReplacer::set_filename(std::string filename){ this->_filename = filename; }
-void StringReplacer::set_to_find(std::string haystack){ this->_to_find = haystack; }
-void StringReplacer::set_to_replace(std::string needle){ this->_to_replace = needle; }
-void StringReplacer::set_result(std::string result){ this->_result = result; }
+void StringReplacer::set_if_filename(std::string filename) { this->_if_filename = filename; }
+void StringReplacer::set_to_find(std::string haystack) { this->_to_find = haystack; }
+void StringReplacer::set_to_replace(std::string needle) { this->_to_replace = needle; }
+void StringReplacer::set_result(std::string result) { this->_result = result; }
 
-std::string StringReplacer::get_filename(void) { return this->_filename; }
+std::string StringReplacer::get_if_filename(void) { return this->_if_filename; }
 std::string StringReplacer::get_to_find(void) { return this->_to_find; }
 std::string StringReplacer::get_to_replace(void) { return this->_to_replace; }
-std::string StringReplacer::get_result(void){ return this->_result; }
+std::string StringReplacer::get_result(void) { return this->_result; }
 
+/**
+ * TODO:
+ * - copy the content to a new file <filename>.replace
+ * - replace every occurrence of s1 witth s2
+ */
 std::string StringReplacer::replace_str(void)
 {
 	std::string line;
 	std::ifstream file;
-	if (file.is_open()) {
-		std::cerr << "Unable to read file " << this->_filename << std::endl;
+	std::ofstream replaced_file("output.txt");
+
+	file.open(this->_if_filename.c_str());
+	if (!file.is_open())
+	{
+		std::cerr << "Unable to read file " << this->_if_filename << std::endl;
+		return "";
 	}
-	file.open(this->_filename.c_str());
 	this->_result.clear();
-	
+
 	while (std::getline(file, line))
 	{
 		std::size_t flag = line.find(this->get_to_find());
-		if (flag != std::string::npos) {
+		if (flag != std::string::npos)
+		{
 			std::string sbstr1 = line.substr(0, flag);
 			std::string sbstr2 = line.substr(flag + this->get_to_find().length(), line.length());
 			std::string final = sbstr1 + this->get_to_replace() + sbstr2;
 			this->set_result(this->get_result() + final + "\n");
-		} else {
+		}
+		else
+		{
 			this->set_result(this->get_result() + line + "\n");
 		}
 	}
-	std::cout << this->get_result();
 	file.close();
-	return "";
+	if (!replaced_file.is_open())
+	{
+		std::cerr << "Unable to open output file" << std::endl;
+		return "";
+	}
+	replaced_file << this->get_result();
+	replaced_file.close();
+	// std::cout << this->get_result();
+	return this->_result;
 }
--- ex04/stringReplacer.hpp
+++ ex04/stringReplacer.hpp
@@ -6,7 +6,7 @@
 /*   By: luciama2 <luciama2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:26:36 by luciama2          #+#    #+#             */
-/*   Updated: 2024/10/09 19:32:17 by luciama2         ###   ########.fr       */
+/*   Updated: 2024/11/06 20:55:00 by luciama2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 class StringReplacer
 {
 	private:
-		std::string _filename;
+		std::string _if_filename;
 		std::string _to_find;
 		std::string _to_replace; //string to replace in haystack
 		std::string _result; //replaced haystack
@@ -29,11 +29,11 @@ class StringReplacer
 		StringReplacer(void);
 		~StringReplacer(void);
 		StringReplacer(std::string filename, std::string to_find, std::string to_replace);
-		void set_filename(std::string filename);
+		void set_if_filename(std::string filename);
 		void set_to_find(std::string haystack);
 		void set_to_replace(std::string needle);
 		void set_result(std::string result);
-		std::string get_filename(void);
+		std::string get_if_filename(void);
 		std::string get_to_find(void);
 		std::string get_to_replace(void);
 		std::string get_result(void);
